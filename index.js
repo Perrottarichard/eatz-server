2527479f1f4859d271563a247ac320e3cb086488
Richard Perrotta <perrottarichard@gmail.com>
2020-10-19 17:34:39 +0700

    sec false

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ app.use(session({
   resave: false,
   saveUninitialized: false,
   store: new MongoStore({ mongooseConnection: mongoose.connection }),
-  cookie: { sameSite: 'none', secure: true }
+  cookie: { sameSite: 'none', secure: false }
 }))
 
 //passport middleware
