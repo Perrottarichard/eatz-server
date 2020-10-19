389d0250f0ad0a491db1d1837a1c756f462c7d68
Richard Perrotta <perrottarichard@gmail.com>
2020-10-19 12:36:59 +0700

    cookie problems

--- index.js
+++ index.js
@@ -54,6 +54,7 @@ app.use(session({
   secret: 'keyboard cat',
   resave: false,
   saveUninitialized: false,
+  cookie: { secure: true },
   store: new MongoStore({ mongooseConnection: mongoose.connection })
 }))
 
